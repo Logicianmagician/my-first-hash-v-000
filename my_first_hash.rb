8880ac00b8f40b286eec0f312920004f19eba3cc
Andrew Kairys <andrewkairys@gmail.com>
2018-03-08 17:21:44 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -4,8 +4,12 @@ end
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
 
-def shipping_manifest(the_manifest)
-  the_manifest = {"whale bone corsets" => 5, "oil paintings" => 3, "porcelain vases" => 2}
+def shipping_manifest
+  the_manifest = {
+    "whale bone corsets" => 5,
+    "oil paintings" => 3,
+    "porcelain vases" => 2
+  }
 end
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information:
